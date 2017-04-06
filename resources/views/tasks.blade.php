e32e66fdddcb5a2fa79e07edcd62eff05e8e613f
olivierwen <olivier@reivilo.net>
2017-04-06 19:03:42 +0000

    route tasks with view

---
+++ resources/views/tasks.blade.php
@@ -0,0 +1,1 @@
+Tasks route {{ $param }}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks/{id?}', function($id = null) {
+   return view('tasks')->with('param', $id);
+});

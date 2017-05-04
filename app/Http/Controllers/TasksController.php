d780e1d768d5ee915cce6a53014b9cbf96a28d61
olivierwen <olivier@reivilo.net>
2017-05-04 18:51:02 +0000

    fix create taskk

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -1,42 +1,32 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 use App\Task;
 use App\Http\Requests\TaskStoreRequest;
-
 class TasksController extends Controller
 {
-    private $tasks;
-    public function __construct() {
-        $this->tasks = collect([
-        ['id' => 2, 'name' => 'Learn Laravel', 'duration' => 12],
-        ['id' => 3, 'name' => 'Learn RubyOnRails', 'duration' => 24]
-        ])->keyBy('id');
+    
+    public function index( Request $request ) {
+        
+        $done = $request->input('done', false);
+        $tasks = Task::done($done)->orderByDesc('created_at')->get();
+        return view('tasks.index')->with('tasks', $tasks);
     }
     
-    public function index()
-    {
-        //return view('tasks.index')->with('tasks', $this->tasks);
-        return view('tasks.index')->with('tasks', Task::all()->where('done',1));
-    }
-    
-    
-    public function show($id)
-    {
-        //return view('tasks.show')->with('task', $this->tasks[$id]);
+    public function show ( $id ) {
+        
+        // return view('tasks.show')->with('task', $this->tasks[$id]); 
         $task = Task::find($id);
         return view('tasks.show')->with('task', $task);
     }
     
-    public function create()
-    {
+    public function create () {
+        
         return view('tasks.create');
     }
     
-    public function store( TaskStoreRequest $request) {
+    public function store ( TaskStoreRequest $request ) {
+        
         $task = Task::create($request->all());
         return redirect(route('tasks.index'));
     }
